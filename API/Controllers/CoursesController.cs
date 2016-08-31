0ac4785213775fdff341a021eb93b8fcc139200a
Stinni <kristinnf13@ru.is>
2016-08-31 21:25:49 +0000

    Multi project up and running

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using A02.Models;
+
+namespace A02.API.Controllers
+{
+    [Route("api/courses")]
+    public class CoursesController
+    {
+        [HttpGet]
+        public List<CourseLiteDTO> GetCoursesOnSemester(string semester = null)
+        {
+            return new List<CourseLiteDTO>
+            {
+                new CourseLiteDTO
+                {
+                    ID = 1,
+                    Name = "Web Services",
+                    Semester = "20163"
+                }
+            };
+        }
+    }
+}
---
+++ Models/CourseLiteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace A02.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CourseLiteDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Semester { get; set; }
+    }
+}
